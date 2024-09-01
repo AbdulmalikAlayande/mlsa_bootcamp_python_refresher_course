98e9ff97e99be149e8c44fb9572578746aaa35fc
AbdulmalikAlayande <alaabdulmalik03@gmail.com>
2024-09-01 18:18:08 +0100

    Initial Commit

---
+++ src/tuples.py
@@ -0,0 +1,19 @@
+student_tuple = ("Samuel", "Jackson", 4.50)
+food_tuple = ("Bread", "Akara", "Yam")
+
+print(student_tuple)
+print(food_tuple)
+
+singleton_tuple = ('random-string',)
+print(type(singleton_tuple))
+
+print(student_tuple[1])
+print(student_tuple[-3])
+
+for index in range(0, len(food_tuple)):
+    print(food_tuple[index])
+
+
+
+
+
