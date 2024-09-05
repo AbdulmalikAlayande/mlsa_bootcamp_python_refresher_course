13d48e4b8dfe94a8fd76022792bc87252dd6ed37
AbdulmalikAlayande <alaabdulmalik03@gmail.com>
2024-09-05 21:57:17 +0100

    removed the .idea folder

--- src/oop/inheritance/animal.py
+++ src/oop/inheritance/animal.py
@@ -1,32 +1,38 @@
 class Animal:
     
-    def __init__(self):
-        pass
+    def __init__(self, name: str, specie: str):
+        self._name: str = name
+        self._specie: str = specie
     
     
-    def walk(self) -> None:
-        pass
+    def move(self) -> None:
+        print("Moving Like An Animal")
     
     def sleep(self) -> None:
-        pass
+        print("I sleep like an Animal")
     
     def eat(self) -> None:
-        pass
+        print("I eat anything edible and delicious") 
     
     def reproduce(self) -> None:
-        pass
+        print("I am an animal so I reproduce and multiply")
     
     
-
-class Lion(Animal):
-    pass
+class WildCat(Animal):
+    
+    def move(self) -> None:
+        print("I walk on all fours ")
+    
+    def sleep(self) -> None:
+        print("I sleep like a Wild Cat")
 
 class Fish(Animal):
     pass
 
-class Eagle(Animal):
+class Bird(Animal):
     pass
 
 class Insect(Animal):
     pass
+
 
